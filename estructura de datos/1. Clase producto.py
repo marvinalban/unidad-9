86f2e6764efd277bc0a0a4b4d1477720a18690b0
marvin <marvinalban30@gmail.com>
2024-08-24 10:38:04 -0500

    se agrega el proyecto

---
+++ estructura de datos/1. Clase producto.py
@@ -0,0 +1,28 @@
+class Producto:
+    def __init__(self, id_producto, nombre, cantidad, precio):
+        self.id_producto = id_producto  # ID único del producto
+        self.nombre = nombre  # Nombre del producto
+        self.cantidad = cantidad  # Cantidad en inventario
+        self.precio = precio  # Precio del producto
+
+    # Getters y Setters para cada atributo
+    def get_id(self):
+        return self.id_producto
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_cantidad(self):
+        return self.cantidad
+
+    def get_precio(self):
+        return self.precio
+
+    def set_cantidad(self, cantidad):
+        self.cantidad = cantidad
+
+    def set_precio(self, precio):
+        self.precio = precio
+
+    def __str__(self):
+        return f"ID: {self.id_producto}, Nombre: {self.nombre}, Cantidad: {self.cantidad}, Precio: ${self.precio:.2f}"
